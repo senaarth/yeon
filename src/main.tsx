177600131c05ad6e2b3a627d6787dfddd9d2f1b4
senaarth <senaarth@gmail.com>
2023-10-03 12:46:00 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FKZelQ7VZYtARAt2CCvmYHF%2FPlayground%3Fpage-id%3D53%253A6748%26type%3Ddesign%26node-id%3D72-7392%26viewport%3D-72%252C90%252C0.26%26t%3Di3ozQEnOc69knRwB-1%26scaling%3Dmin-zoom%26starting-point-node-id%3D72%253A7392%26mode%3Ddesign"
+    ></iframe>
+  </React.StrictMode>
+);
