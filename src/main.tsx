f334e83e51f555a66ee76a0745f5e36eb1ec54ca
senaarth <senaarth@gmail.com>
2023-10-03 12:46:00 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <div>
+      <h1>yeon</h1>
+      <h2>em breve</h2>
+    </div>
+  </React.StrictMode>
+);
